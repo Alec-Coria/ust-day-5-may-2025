7dfb4196e4eb9c9e361cf8cf87f0e9ce3b70ed20
Alec <alec_coria@hotmail.com>
2025-06-05 17:35:42 +0000

    Fix cors backend

--- Nestjs-work/backend-service1/src/main.ts
+++ Nestjs-work/backend-service1/src/main.ts
@@ -21,7 +21,12 @@ async function bootstrap() {
     .build();
   const documentFactory = () => SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, documentFactory);
-  app.enableCors();
+  app.enableCors({
+  origin: ['http://localhost:3000', 'http://localhost:56861'],
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  });
   await app.listen(process.env.PORT ?? 3001);
 }
 bootstrap();
